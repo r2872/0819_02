34d5feb3f18f3f0c0fa71b1edc3a935b97702b47
김현우 <r2872@naver.com>
2021-08-19 10:52:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a0819_02/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a0819_02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 코틀린 문법 연습용 프로젝트 (UI 작업 X)
+    }
+}
